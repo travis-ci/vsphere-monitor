d6f2dc302ed4c8f146f86abf1b440b6d57eab309
Henrik Hodne <henrik@travis-ci.com>
2017-03-22 14:59:07 +0100

    Change reporting to report to Librato every minute

--- main.go
+++ main.go
@@ -1,16 +1,19 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
 	"net/url"
 	"os"
+	"time"
 
 	"github.com/vmware/govmomi"
 	"github.com/vmware/govmomi/find"
 	"github.com/vmware/govmomi/vim25/methods"
-	"github.com/vmware/govmomi/vim25/mo"
 	"github.com/vmware/govmomi/vim25/types"
 )
 
@@ -43,32 +46,80 @@ func main() {
 		log.Fatal("no alarm manager")
 	}
 
-	alarmMap := make(map[string]mo.Alarm)
+	c := time.Tick(time.Minute)
 
-	for _, host := range hosts {
-		fmt.Printf("%s:\n", host.Name())
-		alarmStateResp, err := methods.GetAlarmState(ctx, client.Client, &types.GetAlarmState{This: *alarmManager, Entity: host.Reference()})
-		if err != nil {
-			fmt.Printf("\tcouldn't get alarm states: %v\n", err)
-			continue
-		}
+	for now := range c {
+		metrics := make(map[string]map[string]int64)
 
-		for _, state := range alarmStateResp.Returnval {
-			alarm, ok := alarmMap[state.Alarm.Value]
-			if !ok {
-				err := client.RetrieveOne(ctx, state.Alarm, nil, &alarm)
-				if err != nil {
-					fmt.Printf("couldn't get alarm info for %v\n", state.Alarm)
-					continue
-				}
-				alarmMap[state.Alarm.Value] = alarm
-			}
-
-			if state.OverallStatus == "gray" {
+		for _, host := range hosts {
+			alarmStateResp, err := methods.GetAlarmState(ctx, client.Client, &types.GetAlarmState{This: *alarmManager, Entity: host.Reference()})
+			if err != nil {
+				fmt.Printf("couldn't get alarm states for host %s: %v\n", host.Name(), err)
 				continue
 			}
 
-			fmt.Printf("\t%v -> %s\n", alarm.Info.Name, state.OverallStatus)
+			for _, state := range alarmStateResp.Returnval {
+				if _, ok := metrics[state.Alarm.Value]; !ok {
+					metrics[state.Alarm.Value] = make(map[string]int64)
+				}
+
+				switch state.OverallStatus {
+				case types.ManagedEntityStatusGreen:
+					metrics[state.Alarm.Value][host.Name()] = 0
+				case types.ManagedEntityStatusYellow:
+					metrics[state.Alarm.Value][host.Name()] = 1
+				case types.ManagedEntityStatusRed:
+					metrics[state.Alarm.Value][host.Name()] = 2
+				case types.ManagedEntityStatusGray:
+				}
+			}
+		}
+
+		var libratoMetrics libratoMeasurements
+		libratoMetrics.MeasureTime = now.Unix()
+
+		for name, sourceVals := range metrics {
+			if len(sourceVals) == 0 {
+				continue
+			}
+
+			for source, value := range sourceVals {
+				libratoMetrics.Gauges = append(libratoMetrics.Gauges, libratoGauge{
+					Name:   fmt.Sprintf("travis.vsphere-monitor.host-alarm.%s", name),
+					Value:  float64(value),
+					Source: source,
+				})
+			}
+		}
+
+		body, err := json.Marshal(libratoMetrics)
+		if err != nil {
+			log.Printf("couldn't marshal metrics: %v", err)
+			continue
+		}
+
+		req, err := http.NewRequest("POST", "https://metrics-api.librato.com/v1/metrics", bytes.NewReader(body))
+		if err != nil {
+			log.Printf("couldn't create request: %v", err)
+		}
+
+		req.Header.Set("Content-Type", "application/json")
+		req.SetBasicAuth(os.Getenv("LIBRATO_EMAIL"), os.Getenv("LIBRATO_TOKEN"))
+
+		_, err = http.DefaultClient.Do(req)
+		if err != nil {
+			log.Printf("error sending metrics: %v", err)
 		}
 	}
+}
+
+type libratoMeasurements struct {
+	MeasureTime int64          `json:"measure_time"`
+	Gauges      []libratoGauge `json:"gauges"`
+}
+
+type libratoGauge struct {
+	Name   string  `json:"name"`
+	Value  float64 `json:"value"`
+	Source string  `json:"source"`
 }
