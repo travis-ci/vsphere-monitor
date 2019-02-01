c9bd5429107073eca2f42abb88fe211fb2d56662
Matt Moriarity <matt@mattmoriarity.com>
2019-02-01 14:39:13 -0600

    Use jupiter-brain's ClientProvider for automatic reauth (#13)

--- vsphere.go
+++ vsphere.go
@@ -5,7 +5,7 @@ import (
 	"net/url"
 
 	"github.com/pkg/errors"
-	"github.com/vmware/govmomi"
+	"github.com/travis-ci/jupiter-brain/pkg/vsphereutil"
 	"github.com/vmware/govmomi/find"
 	"github.com/vmware/govmomi/object"
 	"github.com/vmware/govmomi/vim25/methods"
@@ -14,7 +14,7 @@ import (
 
 // A VSphereClient allows for communicating with the vSphere SDK API.
 type VSphereClient struct {
-	client *govmomi.Client
+	clientProvider vsphereutil.ClientProvider
 }
 
 // NewVSphereClient creates a new vSphere Client, connects to the API and
@@ -23,17 +23,19 @@ type VSphereClient struct {
 // "https://admin:password@192.0.2.1/sdk". The insecure argument determines
 // whether the TLS certificate of the SDK endpoint should be verified or not.
 func NewVSphereClient(ctx context.Context, url *url.URL, insecure bool) (*VSphereClient, error) {
-	client, err := govmomi.NewClient(ctx, url, insecure)
-	if err != nil {
-		return nil, errors.Wrap(err, "error creating govmomi client")
-	}
-
-	return &VSphereClient{client: client}, nil
+	return &VSphereClient{
+		clientProvider: vsphereutil.NewClientProvider(url, insecure),
+	}, nil
 }
 
 // ListHostsInCluster returns a list of hosts in the cluster at the given inventory path. Note that the cluster path should be a full inventory path, for example "/datacenter-name/host/cluster-name". If an error occurs, a nil slice is returned with the error.
 func (vc *VSphereClient) ListHostsInCluster(ctx context.Context, clusterPath string) ([]*VSphereHost, error) {
-	finder := find.NewFinder(vc.client.Client, true)
+	client, err := vc.clientProvider.Get(ctx)
+	if err != nil {
+		return nil, errors.Wrap(err, "error creating govmomi client")
+	}
+
+	finder := find.NewFinder(client.Client, true)
 	govmomiHosts, err := finder.HostSystemList(ctx, clusterPath)
 	if err != nil {
 		return nil, errors.Wrap(err, "error listing hosts")
@@ -49,12 +51,17 @@ func (vc *VSphereClient) ListHostsInCluster(ctx context.Context, clusterPath str
 
 // ListAlarmStatesForHost returns a map with the alarm IDs of the alarms defined on the given host as keys, and their current status as the value. Valid statuses are "green", "yellow", "red" and "gray". If an error occurs, it is returned and the map is nil.
 func (vc *VSphereClient) ListAlarmStatesForHost(ctx context.Context, host *VSphereHost) (map[string]string, error) {
-	alarmManager := vc.client.Client.ServiceContent.AlarmManager
+	client, err := vc.clientProvider.Get(ctx)
+	if err != nil {
+		return nil, errors.Wrap(err, "error creating govmomi client")
+	}
+
+	alarmManager := client.Client.ServiceContent.AlarmManager
 	if alarmManager == nil {
 		return nil, errors.New("client has no alarm manager")
 	}
 
-	alarmStateResp, err := methods.GetAlarmState(ctx, vc.client.Client, &types.GetAlarmState{
+	alarmStateResp, err := methods.GetAlarmState(ctx, client.Client, &types.GetAlarmState{
 		This:   *alarmManager,
 		Entity: host.hostSystem.Reference(),
 	})
